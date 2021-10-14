111478654d9c64ccb43513301c6e664063dd6c40
hrjugar <hernanjugar123@gmail.com>
2021-10-14 22:24:42 +0800

    Added and implemented primary font

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Comfortaa',
         primarySwatch: Colors.blue,
       ),
       home: LoginScreen(),
