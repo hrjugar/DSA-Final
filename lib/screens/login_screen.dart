5cb8c127359ca91cb7cb8798511abe608678ca04
hrjugar <hernanjugar123@gmail.com>
2021-10-15 19:24:19 +0800

    Added login fields and button, changed colors

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,39 +9,122 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: const EdgeInsets.all(18),
-        decoration: BoxDecoration(color: Colors.white),
-        child: Column(
-          children: [
-            SizedBox(height: 75),
-            Text("deliveroo",
-              style: TextStyle(
-                color: Colors.deepOrange,
-                fontSize: 45,
-                fontWeight: FontWeight.w900
-              ),
-            ),
-            SizedBox(height: 50),
-            ToggleSwitch(
-              minWidth: 125,
-              minHeight: 35,
-              fontSize: 12,
-              cornerRadius: 20,
-              totalSwitches: 2,
-              labels: ["Customer", "Restaurant"],
-              activeBgColor: [Colors.deepOrange],
-              inactiveBgColor: Colors.grey[200],
-              onToggle: (index) {
-                // TODO: Backend Work
-              },
+      body: Form(
+        key: _formKey,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: const EdgeInsets.all(18),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.orange, Colors.deepOrange]
             )
-          ],
+          ),
+          child: Column(
+            children: [
+              Spacer(flex: 2),
+              Text("deliveroo",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.w900
+                ),
+              ),
+              Spacer(flex: 2),
+              Text("sign in as:",
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+              SizedBox(height: 10),
+              ToggleSwitch(
+                minWidth: 125,
+                minHeight: 35,
+                fontSize: 12,
+                cornerRadius: 20,
+                totalSwitches: 2,
+                labels: ["customer", "restaurant"],
+                icons: [Icons.restaurant, Icons.business_center],
+                activeBgColor: [Colors.white],
+                activeFgColor: Colors.deepOrange,
+                inactiveBgColor: Colors.orange.withOpacity(0.75),
+                inactiveFgColor: Colors.black.withOpacity(0.5),
+                onToggle: (index) {
+                  // TODO: Backend Work
+                },
+              ),
+              Spacer(flex: 1),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none
+                  ),
+                  hintText: "username",
+                  hintStyle: TextStyle(color: Colors.white70),
+                  prefixIcon: Icon(Icons.person, color: Colors.white70),
+                  prefixIconConstraints: BoxConstraints(minWidth: 75),
+                  filled: true,
+                  fillColor: Colors.black.withOpacity(0.15)
+                ),
+                validator: (value) {
+                  // TODO: Backend Work
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none
+                  ),
+                  hintText: "password",
+                  hintStyle: TextStyle(color: Colors.white70),
+                  prefixIcon: Icon(Icons.lock, color: Colors.white70),
+                  prefixIconConstraints: BoxConstraints(minWidth: 75),
+                  filled: true,
+                  fillColor: Colors.black.withOpacity(0.15)
+                ),
+                validator: (value) {
+                  // TODO: Backend Work
+                },
+              ),
+              Spacer(flex: 1),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // TODO: Backend Work
+                  }, 
+                  child: Text("login",
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    elevation: MaterialStateProperty.all(0),
+                  ),
+                ),
+              ),
+              Spacer(flex: 2)
+            ],
+          ),
         ),
       ),
     );
