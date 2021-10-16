b6e1c3050f1c18cb76ea2b9771c7b4e43c197ea1
hrjugar <hernanjugar123@gmail.com>
2021-10-16 15:00:09 +0800

    Added transition to restaurant menu

--- lib/cards/restaurant_card.dart
+++ lib/cards/restaurant_card.dart
@@ -1,3 +1,4 @@
+import 'package:dsa_final/screens/customer_menu_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dsa_final/database/restaurant.dart';
 
@@ -18,60 +19,88 @@ class _RestaurantCardState extends State<RestaurantCard> {
     return SizedBox(
       width: double.infinity,
       height: 300,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15)
-        ),
-        child: CustomMultiChildLayout(
-          delegate: _RestaurantCardLayoutDelegate(),
-          children: [
-            LayoutId(
-              id: _RestaurantCardSlot.background, 
-              child: ClipRRect(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-                child: widget.restaurant.background
-              )
-            ),
-            LayoutId(
-              id: _RestaurantCardSlot.logo, 
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: FittedBox(
-                    fit: BoxFit.fill,
-                    child: widget.restaurant.logo
-                  )
-                )
-              )
-            ),
-            LayoutId(
-              id: _RestaurantCardSlot.name,
-               child: SizedBox(
-                 width: MediaQuery.of(context).size.width * 0.75,
-                 child: Text(widget.restaurant.name,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis
-                 ),
-               )
-            ),
-            LayoutId(
-              id: _RestaurantCardSlot.address, 
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.75,
-                child: Text(widget.restaurant.address,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (context) => CustomerMenuScreen(
+                restaurant: widget.restaurant
               )
             )
-          ],
-        )
+          );
+        },
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)
+          ),
+          child: CustomMultiChildLayout(
+            delegate: _RestaurantCardLayoutDelegate(),
+            children: [
+              LayoutId(
+                id: _RestaurantCardSlot.background, 
+                child: ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                  child: Hero(
+                    tag: "background",
+                    child: widget.restaurant.background
+                  )
+                )
+              ),
+              LayoutId(
+                id: _RestaurantCardSlot.logo, 
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: Hero(
+                        tag: "logo", 
+                        child: widget.restaurant.logo
+                      )
+                    )
+                  )
+                )
+              ),
+              LayoutId(
+                id: _RestaurantCardSlot.name,
+                 child: SizedBox(
+                   width: MediaQuery.of(context).size.width * 0.75,
+                   child: Hero(
+                     tag: "name",
+                     child: Material(
+                       child: Text(widget.restaurant.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis
+                       ),
+                     ),
+                   ),
+                 )
+              ),
+              LayoutId(
+                id: _RestaurantCardSlot.address, 
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  child: Hero(
+                    tag: "address",
+                    child: Material(
+                      child: Text(widget.restaurant.address,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                )
+              )
+            ],
+          )
+        ),
       ),
     );
   }
