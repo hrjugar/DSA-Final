33bcc03e596e34efdd0d989d22e98a08e13adae6
hrjugar <hernanjugar123@gmail.com>
2021-10-16 12:43:12 +0800

    Added appbar

--- lib/screens/customer_main_screen.dart
+++ lib/screens/customer_main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dsa_final/cards/restaurant_card.dart';
+import 'package:dsa_final/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class CustomerMainScreen extends StatelessWidget {
@@ -7,49 +8,52 @@ class CustomerMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.deepOrange,
+        title: Text("restaurants"),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => LoginScreen()
+                )
+              );
+            }, 
+            icon: Icon(Icons.logout)
+          ),
+        ],
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
         padding: const EdgeInsets.all(18),
         decoration: BoxDecoration(color: Colors.white),
-        child: Column(
-          children: [
-            SizedBox(height: 50),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text("restaurants",
-                style: TextStyle(
-                  color: Colors.deepOrange,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30
-                )
-              ),
-            ),
-            SizedBox(height: 20),
-            Flexible(
-              fit: FlexFit.loose,
-              child: ListView.separated(
-                itemCount: 10, // TODO: Backend Work
-                scrollDirection: Axis.vertical,
-                separatorBuilder: (context, index) => SizedBox(height: 20),
-                itemBuilder: (context, index) {
-                  // TODO: Backend Work
-                  return RestaurantCard(
-                    logo: Image.asset(
-                      "assets/images/restaurant_logo_placeholder.png",
-                      width: 50
-                    ),
-                    background: Image.asset(
-                      "assets/images/restaurant_placeholder.jpg"
-                    ),
-                    name: "Restaurant Name",
-                    address: "Address"
-                  );
-                }
-              ),
-            )
-          ],
-        ),
+        child: Flexible(
+          fit: FlexFit.loose,
+          child: ListView.separated(
+            itemCount: 10, // TODO: Backend Work
+            scrollDirection: Axis.vertical,
+            separatorBuilder: (context, index) => SizedBox(height: 20),
+            itemBuilder: (context, index) {
+              // TODO: Backend Work
+              return RestaurantCard(
+                logo: Image.asset(
+                  "assets/images/restaurant_logo_placeholder.png",
+                  width: 50
+                ),
+                background: Image.asset(
+                  "assets/images/restaurant_placeholder.jpg"
+                ),
+                name: "Restaurant Name",
+                address: "Address"
+              );
+            }
+          ),
+        )
       ),
     );
   }
