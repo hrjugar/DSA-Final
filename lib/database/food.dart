7ae7aa13cd95336e169a5c549cda07079863388c
JCU25 <jcUy@mcm.edu.ph>
2021-10-20 01:32:04 +0800

    adjusted image sizes

--- lib/database/food.dart
+++ lib/database/food.dart
@@ -9,5 +9,5 @@ class Food {
     required this.name,
     required this.price,
     required String imagePath
-  }) : image = Image.asset(imagePath);
+  }) : image = Image.asset(imagePath, height: 115, width: 115,);
 }
